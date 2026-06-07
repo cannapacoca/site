05f810ed36c7b4b6af88635551b5e43cc4f1b3af
Daniel <friggidaniel@gmail.com>
2026-06-07 06:58:27 -0300

    teste gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App.jsx';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 
+
+const redirect = sessionStorage.getItem('redirect');
+
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  window.history.replaceState(null, null, redirect);
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
